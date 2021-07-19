e050e90a24a0f500fa12b3c1732dabc20c5a6666
s770am <s770am@gmail.com>
2021-07-19 08:54:46 -0400

    add html for add recipe page

--- server.rb
+++ server.rb
@@ -13,3 +13,16 @@ end
 get "/about" do
     erb :about
 end
+
+get "/add_recipe_form" do
+    @title = "add_recipe_form"
+    erb :add_recipe_form
+end
+
+post "/add_recipe" do
+    
+
+
+
+    
+end
